a8d8cc035d225363a520a6865fb0959eb560bf03
johndillon773 <johndillon773@gmail.com>
2018-04-01 23:06:15 +0000

    anagram refactor

--- exercises/challenges/easy_1/anagram.rb
+++ exercises/challenges/easy_1/anagram.rb
@@ -2,26 +2,15 @@
 
 class Anagram
   def initialize(word)
-    @match_word = word.downcase
+    @word = word.downcase
   end
   
-  def letters(word)
-    word.downcase.chars.sort
-  end
-  
-  def same_letters?(word)
-    letters(word) == letters(@match_word)
-  end
-  
-  def different_words?(word)
-    word.downcase != @match_word
-  end
-  
-  def is_anagram?(word)
-    same_letters?(word) && different_words?(word)
+  def anagram?(word)
+    word.downcase != @word &&
+    word.downcase.chars.sort == @word.chars.sort
   end
   
   def match(list)
-    list.select { |word| is_anagram?(word) }
+    list.select { |word| anagram?(word) }
   end
 end
